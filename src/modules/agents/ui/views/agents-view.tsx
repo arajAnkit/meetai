7199479a5e0cf0fc914508268df3348df3099dd0
Ankit Rajpoot <marketing.araj@gmail.com>
2025-05-30 09:26:16 +0530

    09: (Modify) - Loading agent to server componet

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,28 +3,29 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
-  const { data, isLoading, isError } = useQuery(
-    trpc.agents.getMany.queryOptions()
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+};
+
+export const AgentsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take few moments"
+    />
   );
-  if (isLoading) {
-    return (
-      <LoadingState
-        title="Loading Agents"
-        description="This may take few moments"
-      />
-    );
-  }
-  if (isError) {
+};
+export const AgentsViewError = () => {
+  return (
     <ErrorState
       title="Error Loading Agents"
       description="Please try agailater"
-    />;
-  }
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+    />
+  );
 };
