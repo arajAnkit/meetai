e49350b0382a454cd30b94f80600131c01d7f7c7
Ankit Rajpoot <marketing.araj@gmail.com>
2025-05-31 14:33:29 +0530

    14: (Added) - Agent page

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -3,6 +3,8 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 
+import { useRouter } from "next/navigation";
+
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
@@ -15,6 +17,7 @@ import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+  const router = useRouter();
   const [filters, setFilters] = useAgentsFilters();
 
   const trpc = useTRPC();
@@ -26,7 +29,11 @@ export const AgentsView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+      />
       <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
@@ -54,7 +61,7 @@ export const AgentsViewError = () => {
   return (
     <ErrorState
       title="Error Loading Agents"
-      description="Please try agailater"
+      description="Please try again later"
     />
   );
 };
